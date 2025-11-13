9c61cf1eb2807612e3c57cb6fff2f4b8c6fcf81f
RK <ruk.k0603@icloud.com>
2025-11-13 16:41:27 +0900

    a

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 {
   const timeline = document.getElementById('timeline');
-  for (let i = 0; i <= 1440; i += 60) {
+  for (let i = 0; i <= 720; i += 30) {
     const divider = document.createElement('div');
     divider.classList.add('divider');
     timeline.appendChild(divider);
-    divider.style.top = ((i / 60) * 60) + "px";
+    divider.style.left = (i) + "px";
   };
 }
