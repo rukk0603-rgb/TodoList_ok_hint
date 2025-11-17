01034d91ee8b36c2a2d1531c05ebb6c715eea74d
RK <ruk.k0603@icloud.com>
2025-11-17 21:10:45 +0900

    a

--- main.js
+++ main.js
@@ -2,10 +2,24 @@
 
 {
   const timeline = document.getElementById('timeline');
-  for (let i = 0; i <= 720; i += 30) {
+  for (let i = 0; i <= 24; i++) {
     const divider = document.createElement('div');
     divider.classList.add('divider');
+    divider.style.top = (i * 100) + "px"; 
     timeline.appendChild(divider);
-    divider.style.left = (i) + "px";
+
+  for (let i = 0; i <= 48; i++) {
+    const halfDivider = document.createElement('div');
+    halfDivider.classList.add('halfD');
+    halfDivider.style.top = ((i * 100) / 2) + "px";
+    timeline.appendChild(halfDivider);
+  }
+  
+    const label = document.createElement('span');
+    label.textContent = `${i}:00`;
+    label.style.position = 'absolute';
+    label.style.left = '100px';
+    label.style.top = (i * 100 - 8) + "px";
+    timeline.appendChild(label);
   };
 }
